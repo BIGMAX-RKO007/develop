c1598fff1aa01ff6d47f450a28b4e2fc69e5818b
fanxiao <369246926@qq.com>
2022-03-31 17:49:37 +0800

    develop-text

--- src/main/java/com/flink/testes/test1.java
+++ src/main/java/com/flink/testes/test1.java
@@ -6,14 +6,49 @@ import java.util.*;
 
 public class test1 {
     public static void main(String[] args) {
-        long l = System.currentTimeMillis();
+        Map map = new HashMap();
+        map.put("111",1);
+        map.put("111",2);
+        map.put("111",3);
+        map.put("222",21);
+        map.put("222",22);
+        map.put("222",23);
+        System.out.println(map);
+        if (map.get("111").equals(1)){
+            map.put("111",1111);
+        }else if (map.get("111").equals(2)){
+            map.put("111",2222);
+        }else{
+            map.put("111",3333);
+        }
+        System.out.println(map);
+        if (map.get("222").equals(21)){
+            map.put("222",2323);
+        }else if (map.get("222").equals(22)){
+            map.put("222",2424);
+        }else{
+            map.put("222",2525);
+        }
+        System.out.println(map);
+
+
+
+
+
+
+
+
+
+
+
+        /*long l = System.currentTimeMillis();
         System.out.println(l);
         long date = new Date().getTime();
         System.out.println(date);
 
 
         String s = IdUtil.simpleUUID();
-        System.out.println(s);
+        System.out.println(s);*/
 
 
 
