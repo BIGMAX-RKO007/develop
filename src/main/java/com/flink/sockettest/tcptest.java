72d99b5f28839152054a6de09686d236c2156106
樊霄 <fanxiao@iie.ac.cn>
2022-09-11 19:31:14 +0800

    123

--- src/main/java/com/flink/sockettest/tcptest.java
+++ src/main/java/com/flink/sockettest/tcptest.java
@@ -9,12 +9,12 @@ public class tcptest {
         public static void main(String[] args) throws IOException {
             //创建客户端的Socket对象(Socket)
             //Socket(String host, int port) 创建流套接字并将其连接到指定主机上的指定端口号
-            Socket s = new Socket("10.10.41.251",20186);
+            Socket s = new Socket("10.10.42.241",20188);
 
             //获取输出流，写数据
             //OutputStream getOutputStream() 返回此套接字的输出流
             OutputStream os = s.getOutputStream();
-            os.write("hello,tcp,我来了".getBytes());
+            os.write("hello,tcp".getBytes());
 
             //释放资源
             s.close();
